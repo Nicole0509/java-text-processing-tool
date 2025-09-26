7b07cfcd00a1f27604e4c29f0c462aa7c0ffc88f
Nicole0509 <niashavril0509@gmail.com>
2025-09-26 13:41:21 +0200

    Officailly started designing the app

--- src/main/java/org/example/TheRealMainClass.java
+++ src/main/java/org/example/TheRealMainClass.java
@@ -1,23 +1,25 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.regex.Pattern;
 
 public class TheRealMainClass extends Application {
 
-    static int choice;
+    private final TextField  inputChoice  = new TextField();
+    private TextField  enterText = new TextField();
 
-    public static Set<String> textProcessingSet = new HashSet<String>();
+    private TableView<String> tableView = new TableView<>();
+    private final ObservableList<String[]> data = FXCollections.observableArrayList();
 
     public static void main(String[] args) {
         Application.launch(TheRealMainClass.class, args);
@@ -25,50 +27,10 @@ public class TheRealMainClass extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        var promptLabel = new Label("Choose An Option:");
-        var inputChoice = new TextField();
-        var enterChoiceButton = new Button("Submit");
-        var feedBack = new Label();
-
-        var layout = new VBox(10,promptLabel,inputChoice,enterChoiceButton,feedBack);
-
-        var scene = new Scene(layout, 400, 400);
         stage.setTitle("TEXT PROCESSING TOOL");
+
+        final Scene scene = new Scene(inputChoice, 400, 400);
         stage.setScene(scene);
         stage.show();
-
-        enterChoiceButton.setOnAction(e->{
-            try{
-                choice = Integer.parseInt(inputChoice.getText());
-            } catch(Exception ex){
-                choice = -1;
-            }
-
-            switch (choice){
-                case 1:
-                    feedBack.setText("View A list of available Text");
-
-                    break;
-                case 2:
-                    feedBack.setText("Add Text");
-                    break;
-                case 3:
-                    feedBack.setText("Replace Text");
-                    break;
-                case 4:
-                    feedBack.setText("Delete Text");
-                    break;
-                case 5:
-                    feedBack.setText("Exit");
-                    break;
-                case 6:
-                    feedBack.setText("Search Text");
-                    break;
-                default:
-                    feedBack.setText("Invalid choice");
-                    break;
-            }
-
-        });
     }
 }
